fa8d7d55f01784b854b1d762ec62b1073dfa1213
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-03 13:33:27 +0700

    Create user login routes

--- routes/users/controller.js
+++ routes/users/controller.js
@@ -12,5 +12,26 @@ module.exports = {
       // console.log(user);
       res.send('Registration success');
     });
+  },
+  userLogin: (req, res) => {
+    userData = {
+      user_email: req.body.user_email,
+      user_password: req.body.user_password
+    };
+
+    User.findOne({ where: userData })
+      .then(user => {
+        console.log(user);
+
+        const { user_name, user_email } = user.dataValues;
+        res.status(200).send({
+          message: 'user found',
+          data: {
+            user_name,
+            user_email
+          }
+        });
+      })
+      .catch(error => res.send(error));
   }
 };
--- routes/users/index.js
+++ routes/users/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.post('/register', require('./controller').userRegistration);
+router.post('/login', require('./controller').userLogin);
 
 module.exports = router;
