98695949ecd2ff9015e4b3f24c495699c09ff8d6
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-12-03 11:47:19 +0700

    Create model for user

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+const { sequelize } = require('../config');
+
+const User = sequelize.define(
+  'user',
+  {
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      field: 'user_id',
+      autoIncrement: true,
+      primaryKey: true
+    },
+    user_name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: 'user_name'
+    },
+    user_email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: 'user_email'
+    },
+    user_password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: 'user_password'
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      field: 'user_createdAt'
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      field: 'user_updatedAt'
+    }
+  },
+  {
+    tableName: 'users'
+  }
+);
+
+module.exports = User;
